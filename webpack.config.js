c2254c0ded7217904ef78d9d92e52bd92262c0de
chapmancheng <chapboy1989@gmail.com>
2021-01-20 02:03:08 +0800

    made umd and var webpack output

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,53 @@
-const path = require('path');
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
-module.exports = {
-  mode: "production",
-  context: path.resolve(__dirname),
-  entry: './src/index.ts',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'index.js',
-    library: 'hansifyNumbers',
-    libraryTarget: 'umd',
-    globalObject: 'this',
-    libraryExport: 'default' 
-  },
-  resolve: {
-    extensions: [".ts", ".tsx", ".js"]
-  },
+module.exports = function (env, argv) {
+  const outputs = [
+    {
+      libraryTarget: "umd",
+      globalObject: "this",
+    },
+    {
+      libraryTarget: "var",
+      globalObject: "window",
+    },
+  ];
 
-  module: {
-    rules: [
-      {
-        test: /\.m?[j|t]s$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: [
-              '@babel/preset-env', 
-              "@babel/preset-typescript"
-            ]
-          }
-        }
-      },
-      // {
-      //   test: /\.tsx?$/,
-      //   loader: 'ts-loader'
-      // }
-    ]
-  },
-  devtool: 'source-map'
+  const isProduction = argv.mode === /prod(uction)?/;
+  
+  return outputs.map(({globalObject, libraryTarget}) => ({
+    mode: isProduction ? 'production' : 'development',
+    context: path.resolve(__dirname),
+    entry: "./src/index.ts",
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: `index.${libraryTarget}.js`,
+      library: "hansifyNumbers",
+      libraryTarget,
+      libraryExport: "default",
+      globalObject,
+    },
+    resolve: {
+      extensions: [".ts", ".tsx", ".js"],
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.m?[j|t]s$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env", "@babel/preset-typescript"],
+            },
+          },
+        },
+      ],
+    },
+    plugins: [
+      new CleanWebpackPlugin({ cleanOnceBeforeBuildPatterns: [], }),
+    ],
+    devtool: isProduction ? "" : "source-map",
+  }));
 };
-
-// TODO: add sourcemap
-// TODO: add libraryTarget: var
